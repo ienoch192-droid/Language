bd4d009217e5fe34ad891dbcb853baf763c6367f
ienoch192-droid <ienoch192@gmail.com>
2026-01-17 19:19:16 +0100

    Add files via upload

---
+++ app.py.py
@@ -0,0 +1,24 @@
+mwaghavul = {
+             "goodmorning": "teerianga",
+             "God": "naan",
+             "food": "biise",
+             "welcome": "mwale",
+             "cane": "kam",
+             "bible": "po naan",
+             "song": "koghg",
+             "chair": "bi tong",
+             "food": "bisee",
+             "medicine": "yen",
+             "cloud": "nluu",
+             "please": "kusugk",
+             "salt": "kiin",
+             "run": "zsu",
+             "come": "ji",
+             "go": "so",
+             "friend": "shaarr",
+             "money": "shagal",
+             "how are you": "aaere",
+             "God is good": "da naaan diret",
+             "what is your name?": "suum fugha a wee"}
+
+
